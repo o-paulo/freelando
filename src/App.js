57a14904f857320df6a17e166e88ed8c03ff4083
o-paulo <paulo.pereira707@outlook.com>
2024-05-16 13:13:07 -0300

    feat: criacao do card e temas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Card } from "./componentes/Card/Card";
 import { EstilosGlobais } from "./componentes/EstilosGlobais/Estilos";
+import { ProvedorTema } from "./componentes/ProvedorTema/ProvedorTema";
 
 function App() {
   return (
-    <div>
+    <ProvedorTema>
       <EstilosGlobais />
       <Card>
         <h1>Freelando</h1>
       </Card>
-    </div>
+    </ProvedorTema>
   );
 }
 
